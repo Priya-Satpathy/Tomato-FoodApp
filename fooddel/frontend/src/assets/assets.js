79066948aebd67e3c2f4e3a1af873d937a38f66c
Priya-Satpathy <priyasatpathy429@gmail.com>
2025-10-27 21:01:08 +0530

    content add

--- fooddel/frontend/src/assets/assets.js
+++ fooddel/frontend/src/assets/assets.js
@@ -22,6 +22,7 @@ import food_3 from './food_3.png'
 import food_4 from './food_4.png'
 import food_5 from './food_5.png'
 import food_6 from './food_6.png'
+
 import food_7 from './food_7.png'
 import food_8 from './food_8.png'
 import food_9 from './food_9.png'
